a42f8fa280e96947021a597f27ae3e51ca977716
aydenmason <aydenjmason@gmail.com>
2019-11-06 14:27:40 -0600

    adding todo list and a login page

--- driver.py
+++ driver.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'god' or request.form['password'] != 'god':
+            error = 'Username or password not recognized. Try Again'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 @app.route("/")
 def home():
