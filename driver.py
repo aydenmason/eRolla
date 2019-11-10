989c2ad878eafd8a9e9613e73a03b2146295555f
aydenmason <aydenjmason@gmail.com>
2019-11-10 00:49:02 -0600

    updating driver

--- driver.py
+++ driver.py
@@ -1,10 +1,53 @@
 from flask import Flask, render_template, redirect, url_for, request
-
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
 app = Flask(__name__)
 
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] ='project123!'
+app.config['MYSQL_HOST'] ='localhost'
+app.config['MYSQL_DB'] ='project'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
+mysql = MySQL(app)
 
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = form.username.data        
+        password = sha256_crypt.encrypt(str(form.password.data))
+        name = form.name.data
+        email = form.email.data
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute("INSERT INTO user(username, password, name, email) VALUES(%s, %s, %s, %s)", (username, password, name, email))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', title ="Register", form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -16,7 +59,7 @@ def login():
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template("home.html")
     
