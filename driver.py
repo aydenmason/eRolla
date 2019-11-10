cd66f180345f86e28706387372980535e88e0c9b
aydenmason <aydenjmason@gmail.com>
2019-11-10 00:49:41 -0600

    updating driver

--- driver.py
+++ driver.py
@@ -19,7 +19,7 @@ class RegisterForm(Form):
     email = StringField('Email', [validators.Length(min=6, max=50)])
     password = PasswordField('Password', [
         validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
+        validators.EqualTo('confirm', message='Passwords dont match')
     ])
     confirm = PasswordField('Confirm Password')
 
